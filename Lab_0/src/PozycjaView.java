805a6e37b60259e148cf48cc57e0edc04d144c48
Vladyslav Kavetskyy <50554574+vkavetskyy@users.noreply.github.com>
2020-04-22 10:55:28 +0200

    upd

--- Lab_0/src/PozycjaView.java
+++
@@ -1,7 +0,0 @@
-public class PozycjaView {
-    public void printPozycja(String nazwa_pozycji, int ilosc, float cena) {
-        System.out.println(nazwa_pozycji);
-        System.out.println(ilosc);
-        System.out.println(cena);
-    }
-}
